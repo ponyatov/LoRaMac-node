f7441ee05061e038843dc41bba5b12f131fba29c
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-27 17:49:32 +0400

    + EBYTE E19 series
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/boards/EBYTE_E19/README.md
@@ -0,0 +1,6 @@
+# EBYTE E19
+
+Stand-slone SX1276 modules with SPI interface:
+
+- E19-868M20S
+- E19-868M30S
---
+++ src/boards/EBYTE_E19/board.c
@@ -0,0 +1,24 @@
+/*!
+ * \file      board.c
+ *
+ * \brief     EBYTE E19 series Target board general functions implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \author    Dmitry Ponyatov <dponyatov@gmail.com
+ */
+
+//#include "stm32l1xx.h"
+//#include "utilities.h"
+//#include "delay.h"
+//#include "gpio.h"
+//#include "adc.h"
+//#include "spi.h"
+//#include "i2c.h"
+//#include "uart.h"
+//#include "timer.h"
+//#include "gps.h"
+#include "board-config.h"
+//#include "rtc-board.h"
+#include "sx1276-board.h"
+#include "board.h"
